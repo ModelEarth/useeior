1dd7daeb4d16f93f99259db5d1bb02c09ea44a31
MoLi7 <mo.vico.li@gmail.com>
2020-05-28 13:49:47 -0700

    Create and document generateDomestic2RegionICFs function

--- R/InteregionalCommodityFlowFunctions.R
+++ R/InteregionalCommodityFlowFunctions.R
@@ -37,3 +37,72 @@ calculateLocalandTradedRatios <- function (state, year, SoI = TRUE, ioschema, io
   LocalandTradedRatiosbyBEA <- LocalorTraded[, c(bea_code, "LocalRatio", "TradedRatio")]
   return(LocalandTradedRatiosbyBEA)
 }
+
+#' Generate domestic 2 region inter-regional commodity flows (ICFs) table.
+#' @param state State name.
+#' @param year A numeric value between 2012 and 2017 specifying the year of interest.
+#' @param remove_scrap A boolean variable indicating whether to remove scrap from the table.
+#' @param ioschema A numeric value of either 2012 or 2007 specifying the io schema year.
+#' @param iolevel BEA sector level of detail, can be "Detail", "Summary", or "Sector".
+#' @return A data frame contains domestic 2 region ICFs.
+generateDomestic2RegionICFs <- function (state, year, remove_scrap = FALSE, ioschema, iolevel) {
+  # Specify BEA code
+  bea_code <- paste("BEA", ioschema, iolevel, "Code", sep = "_")
+  # Generate SoI-RoUS commodity flow ratios
+  ICF_2r <- calculateCommodityFlowRatios(state, year, "domestic", ioschema, iolevel)
+  ICF_2r$flowpath <- paste0(ICF_2r$ORIG, "2", ICF_2r$DEST)
+  ICF_2r_wide <- reshape2::dcast(ICF_2r[, c(bea_code, "ratio", "flowpath")],
+                                 paste(bea_code, "~", "flowpath"), value.var = "ratio")
+  
+  # Calculate interregional commodity flow (ICF) ratios for all commodities
+  # Merge ICF_2r_wide with complete BEA Commodity list
+  ICF_comm <- merge(ICF_2r_wide, get(paste(iolevel, "CommodityCodeName_2012", sep = "_")),
+                    by.x = bea_code,
+                    by.y = paste("BEA", ioschema, iolevel, "Commodity_Code", sep = "_"),
+                    all.y = TRUE)
+  # Calculate SoI local and traded ratios
+  localandtratdedratioSoI <- calculateLocalandTradedRatios(state, year, SoI = TRUE, ioschema, iolevel)
+  # Calculate RoUS local and traded ratios
+  localandtratdedratioRoUS <- calculateLocalandTradedRatios(state, year, SoI = FALSE, ioschema, iolevel)
+  # Load SoI Commodity Output
+  filename <- load(paste0("data/State_", iolevel, "_CommodityOutput_", year, ".rda"))
+  StateCommOutput <- get(filename)[[state]]
+  US_Make <- get(paste(iolevel, "Make", year, "BeforeRedef", sep = "_"))*1E6
+  USCommOutput <- colSums(US_Make[-which(rownames(US_Make)=="Total Commodity Output"),
+                                  -which(colnames(US_Make)=="Total Industry Output")])
+  StateCommOutput$SoI_US_ratio <- StateCommOutput[, 1]/USCommOutput
+  # Calculate local and traded ratios
+  for (BEAcode in Reduce(intersect, list(ICF_comm[is.na(ICF_comm$SoI2SoI), bea_code],
+                                         localandtratdedratioSoI[, bea_code],
+                                         localandtratdedratioRoUS[, bea_code]))) {
+    # Assign data source
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "source"] <- "State Supply model"
+    # RoUS2RoUS = LocalRoUS + OR*TradedRoUS
+    LocalRoUS <- localandtratdedratioRoUS[localandtratdedratioRoUS[, bea_code]==BEAcode, "LocalRatio"]
+    OR <- StateCommOutput[BEAcode, "SoI_US_ratio"]
+    TradedRoUS <- localandtratdedratioRoUS[localandtratdedratioRoUS[, bea_code]==BEAcode, "TradedRatio"]
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "RoUS2RoUS"] <- LocalRoUS + OR*TradedRoUS
+    # SoI2SoI = SoI_Local
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "SoI2SoI"] <- localandtratdedratioSoI[localandtratdedratioSoI[, bea_code]==BEAcode, "LocalRatio"]
+    # RoUS2SoI = 1 - SoI2SoI
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "RoUS2SoI"] <- 1 - ICF_comm[ICF_comm[, bea_code]==BEAcode, "SoI2SoI"]
+    # SoI2RoUS = 1 - RoUS2RoUS
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "SoI2RoUS"] <- 1 - ICF_comm[ICF_comm[, bea_code]==BEAcode, "RoUS2RoUS"]
+  }
+  # Use SoI and RoUS commodity output
+  for (BEAcode in intersect(ICF_comm[is.na(ICF_comm$source), bea_code], rownames(StateCommOutput))) {
+    # Assign data source
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "source"] <- "State Commodity Output"
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "RoUS2RoUS"] <- 1 - StateCommOutput[BEAcode, "SoI_US_ratio"]
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "SoI2SoI"] <- StateCommOutput[BEAcode, "SoI_US_ratio"]
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "RoUS2SoI"] <- 1 - StateCommOutput[BEAcode, "SoI_US_ratio"]
+    ICF_comm[ICF_comm[, bea_code]==BEAcode, "SoI2RoUS"] <- StateCommOutput[BEAcode, "SoI_US_ratio"]
+  }
+  
+  ICF_comm[is.na(ICF_comm)] <- 0
+  #note this is not applicable for now because SOO401 is not present
+  if (remove_scrap) {
+    ICF_comm <- ICF_comm[-which(ICF_comm[, bea_code]=="S00401"), ]
+  }
+  return(ICF_comm)
+}
